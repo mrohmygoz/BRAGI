ce1bc5df2faefa78f5d501b4323b01aef0852305
YiJen Peng <mrohmygoz@gmail.com>
2015-12-22 00:19:34 +0800

    2 test commit

--- Bragi/src/main/java/com/uninum/bragi/ui/Activity_Main.java
+++ Bragi/src/main/java/com/uninum/bragi/ui/Activity_Main.java
@@ -13,6 +13,6 @@ public class Activity_Main extends Activity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        //test
+        //test 2
     }
 }
