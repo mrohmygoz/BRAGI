3a99ef9f853a9cc4659f2579c0b1372f4537a70e
YiJen Peng <mrohmygoz@gmail.com>
2015-12-21 23:22:24 +0800

    Initial commit

---
+++ Bragi/src/main/java/com/uninum/bragi/ui/Activity_Main.java
@@ -0,0 +1,17 @@
+package com.uninum.bragi.ui;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+import com.uninum.elite.R;
+
+/**
+ * Created by YiJenPeng on 2015/12/15.
+ */
+public class Activity_Main extends Activity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
